03cf9b86357b4e9e091c3a4ae73d9782f522d9e5
Soprodevento <Sashinka2803@gmail.com>
2024-03-13 20:42:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5,5,6,7,7};
+        printArray(numbers);
+    }
+    public static void printArray(int[] numbers) {
+        System.out.println(Arrays.toString(numbers));
+    }
+
+
+}
+ 
